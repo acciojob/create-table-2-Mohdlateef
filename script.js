6c7d4145b34df5d8a3b4d7ab7bdd794594c62a90
LATEEF AHMAD BABA <91480408+Mohdlateef@users.noreply.github.com>
2024-03-22 22:04:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-function createTable() {
-    //Write your code here
-  
+let table=document.getElementById("table");
+
+let rowss=prompt("Input number of Rows")
+let colus=prompt("Input number of columns")
+
+let rows=+rowss
+let colum=+colus;
+functio createTable() {
+	
+
+for(let i=0;i<rows;i++)
+{
+    let rows=document.createElement("tr");
+    table.appendChild(rows)
+    for(let j=0;j<colum;j++)
+    {
+    let col=document.createElement("td");
+    rows.appendChild(col);
+    col.innerText=`Row-${i}Column-${j}`
+    }
+}
 }
